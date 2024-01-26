2bda4c155cf5e6a95ea20c40cd54aede26440967
alexismrosales <aleisjorge91@gmail.com>
2024-01-26 03:03:14 -0600

    first push of the web app

---
+++ web/src/code/App/index.js
@@ -0,0 +1,12 @@
+import Header from '../Header';
+import Slider from '../Slider';
+import Cards from '../Cards';
+function App() {
+  return ([
+    <Header/>,
+    <Slider/>, 
+    <Cards/>
+  ]);
+}
+
+export default App;
