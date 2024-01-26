6addd194403b172659d3e02e6e906303596f7825
alexismrosales <aleisjorge91@gmail.com>
2024-01-26 12:04:20 -0600

    converting to json

--- api/__init__.py
+++ api/__init__.py
@@ -4,15 +4,15 @@ from analysis import hello
 
 app = Flask(__name__)
 
-# App roote
+# App route
 @app.route('/')
+
 # Return results of analysis
 def create_app():
     data  = hello()
     str = "{\"data\": \"" + data + "\"}"
     return json.loads(str)
+
 # Run app
 if __name__ == "__main__":
     app.run()
-    
-    
