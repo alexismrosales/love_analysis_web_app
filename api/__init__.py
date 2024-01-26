b553e73f3c2bde39e982f72bb928513caf7c1b79
alexismrosales <aleisjorge91@gmail.com>
2024-01-26 12:26:45 -0600

    api config request

--- api/__init__.py
+++ api/__init__.py
@@ -1,17 +1,20 @@
-import json
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
 from analysis import hello
 
 app = Flask(__name__)
 
+# Allowing to use localhost:3000
+cors = CORS(app, origins=['http://localhost:3000'])
+app.config['CORS_HEADERS'] = 'Content-Type'
 # App route
 @app.route('/')
 
 # Return results of analysis
 def create_app():
     data  = hello()
-    str = "{\"data\": \"" + data + "\"}"
-    return json.loads(str)
+    # Return of json file with data
+    return jsonify(data=data)
 
 # Run app
 if __name__ == "__main__":
