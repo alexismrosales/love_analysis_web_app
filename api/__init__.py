029e77acfa2e6a0cdd6a181d0a1d69a931cbd786
alexismrosales <aleisjorge91@gmail.com>
2024-01-28 01:25:01 -0600

    api changes

--- api/__init__.py
+++ api/__init__.py
@@ -1,16 +1,16 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+
 from analysis import hello
 
 app = Flask(__name__)
 
-# Allowing to use localhost:3000
 cors = CORS(app, origins=['http://localhost:3000', 'https://alexismrosales.github.io'])
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 # App route
 @app.route('/')
-
+@cross_origin()
 # Return results of analysis
 def create_app():
     data  = hello()
