0b25b249a40d581d451cbc5506b2cb5953c91b26
alexismrosales <aleisjorge91@gmail.com>
2024-02-05 15:04:40 -0600

    module added to render emojis

--- web/src/code/TopEmojis/TopEmojisItem.js
+++ web/src/code/TopEmojis/TopEmojisItem.js
@@ -1,5 +1,6 @@
 
 import React,{ useEffect } from "react";
+import { Twemoji } from "react-emoji-render";
 import classNames from "classnames";
 
 import TopEmojisData from "../../data/TopEmojisData";
@@ -7,6 +8,7 @@ import TopEmojisData from "../../data/TopEmojisData";
 import CounterAnimation from "../_scripts/counterAnimation";
 
 import style from "./Styles.module.css";
+import './Styles.css'
 
 function TopEmojisItem(){
     const data = TopEmojisData();
@@ -14,18 +16,18 @@ function TopEmojisItem(){
         <div className={style.TopEmojis}>
             <p className={style.Title}>TOP 3 EMOJIS</p>
             <div className={style.EmojisContainer}>
-                <div className={style.TopTwo}>
-                    <span className={style.TopEmoji}> {data.topEmojis[1] ? data.topEmojis[1].emoji : []}</span>
+                <div className={style.TopTwo}>  
+                    <span className={style.TopEmoji}><Twemoji svg text={data.topEmojis[1] ? data.topEmojis[1].emoji : []} onlyEmojiClassName="emoji-span"/></span>
                     <br/><span className={style.TopTitle}>Top 2</span> 
                     <p className={style.TopText}>Se uso <br/><span className={style.Counter1}> <br/><CounterAnimation limit={data.topEmojis[1] ? data.topEmojis[1].count : []} height={95} marginTop={4} /> </span> <span className="pt-3">veces</span></p>
                 </div>
                 <div className={style.TopOne}>
-                    <span className={style.TopEmoji}> {data.topEmojis[0] ? data.topEmojis[0].emoji : []}</span>
+                    <span className={style.TopEmoji}> <Twemoji svg text={data.topEmojis[0] ? data.topEmojis[0].emoji : []} onlyEmojiClassName="emoji-span" /></span>
                     <br/><span className={style.TopTitle}>Top 1</span> 
                     <p className={style.TopText}>Se uso <br/><span className={style.Counter1}> <br/><CounterAnimation limit={data.topEmojis[0] ? data.topEmojis[0].count : []} height={95} marginTop={4} /> </span> <span className="pt-3">veces</span></p>
                 </div>
                 <div className={style.TopThree}>
-                    <span className={style.TopEmoji}> {data.topEmojis[2] ? data.topEmojis[2].emoji : []}</span>
+                    <span className={style.TopEmoji}><Twemoji svg text={data.topEmojis[2] ? data.topEmojis[2].emoji : []} onlyEmojiClassName="emoji-span" /></span>
                     <br/><span className={style.TopTitle}>Top 3</span> 
                     <p className={style.TopText}>Se uso <br/><span className={style.Counter1}> <br/><CounterAnimation limit={data.topEmojis[2] ? data.topEmojis[2].count : []} height={95} marginTop={4} /> </span> <span className="pt-3">veces</span></p>
                 </div>
