c719e5289e99add7ddee4a1e0641617de4a38085
alexismrosales <aleisjorge91@gmail.com>
2024-02-05 21:30:35 -0600

    update emoji view

--- web/src/code/_scripts/counterAnimation.js
+++ web/src/code/_scripts/counterAnimation.js
@@ -7,7 +7,7 @@ function CounterAnimation({limit, time, height , marginTop, marginBottom}) {
       <VisibilitySensor partialVisibility offset={{ bottom: 150 }}>
         {({ isVisible }) => (
           <div style={{ height: height ? height : 50}} className={`relative items-center justify-center -mt-${marginTop ? marginTop : '0'} -mb-${marginBottom ? marginBottom : '0'}`}>
-            {isVisible ? <CountUp end={limit} duration={time ? time : 20} /> : null}
+            {isVisible ? <CountUp end={limit} duration={time ? time : 16} /> : null}
           </div>
         )}
       </VisibilitySensor>
