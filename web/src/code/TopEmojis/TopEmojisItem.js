d5c0eaf58975877598290512b780c30b64942b2e
alexismrosales <aleisjorge91@gmail.com>
2024-02-05 20:27:11 -0600

    add info emoji view

--- web/src/code/TopEmojis/TopEmojisItem.js
+++ web/src/code/TopEmojis/TopEmojisItem.js
@@ -1,7 +1,6 @@
 
-import React,{ useEffect } from "react";
+import React from "react";
 import Emoji, { Twemoji } from "react-emoji-render";
-import classNames from "classnames";
 
 import TopEmojisData from "../../data/TopEmojisData";
 
