f6d336f4d37b100ba697ba93b90cb310fdddc340
alexismrosales <aleisjorge91@gmail.com>
2024-02-07 00:15:55 -0600

    update Talkinfo view

--- web/src/code/TalkInfo/TalkInfoItem.js
+++ web/src/code/TalkInfo/TalkInfoItem.js
@@ -1,14 +1,38 @@
 import React from "react";
+import translate from "translate"
+
+import TalkInfoData from "../../data/TalkInfoData";
 
 import Style from "./Styles.module.css";
 
+
+
 function TalkInfoItem()
 {
+    
+    // Mover el hook a esta función
+    const [translatedData, setTranslatedData] = React.useState(null);
+
+    // Obtener los datos de TalkInfoData
+    const data = TalkInfoData();
+
+    React.useEffect(() => {
+        // Llamar a la función asíncrona y actualizar el estado
+        getTranslatedData(data).then(data => setTranslatedData(data));
+    }, []);
+
     return(
         <div>
             <div className={Style.DateInfo}>
                 <div className={Style.DateImgDiv}><img src={process.env.PUBLIC_URL+"/img/hourglass.png"} className={Style.DateImg}/></div>
-                <div>text</div>
+                <div>
+                    <p>¿Cuando hablamos más? </p>
+                    <ul>
+                        <li>¿Sabías que el día en que mas hablamos es el {translatedData?.day}? 😯 </li>
+                        <li>Y nuestro el mes donde más hablamos fue en {translatedData?.month} 😳</li>
+                        <li>Así como la hora donde hablamos más durante el día es a las {data.variousData[1] ? data.variousData[1].hour : []}:00 😚</li>
+                    </ul>
+                </div>
             </div>
             <div className={Style.WTMInfo}>
                 <div>text</div>
@@ -21,4 +45,20 @@ function TalkInfoItem()
         </div>
     )
 }
+
+
+async function getTranslatedData(data) {
+    translate.engine = "google";
+  
+    const day = await translate(
+      data.variousData[0] ? data.variousData.day : "sunday",
+      "es"
+    );
+    const month = await translate(
+      data.variousData[2] ? data.variousData.month : "august",
+      "es"
+    );
+    return { day, month };
+}
+
 export default TalkInfoItem;
--- web/src/code/_scripts/counterAnimation.js
+++ web/src/code/_scripts/counterAnimation.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import CountUp from 'react-countup';
+import CountUp, { useCountUp } from 'react-countup';
 import VisibilitySensor from 'react-visibility-sensor';
 
 function CounterAnimation({limit, time, height , marginTop, marginBottom}) {
   return (
-      <VisibilitySensor partialVisibility offset={{ bottom: 150 }}>
-        {({ isVisible }) => (
-          <div style={{ height: height ? height : 50}} className={`relative items-center justify-center -mt-${marginTop ? marginTop : '0'} -mb-${marginBottom ? marginBottom : '0'}`}>
-            {isVisible ? <CountUp end={limit} duration={time ? time : 16} /> : null}
-          </div>
-        )}
+    <CountUp end={limit} duration={time ? time : 13}>
+    {({ countUpRef, start }) => (
+      <VisibilitySensor onChange={start} partialVisibility offset={{ bottom: 150 }}>
+        <span ref={countUpRef} style={{ height: height ? height : 50}} className={`relative items-center justify-center -mt-${marginTop ? marginTop : '0'} -mb-${marginBottom ? marginBottom : '0'}`} />
       </VisibilitySensor>
+    )}
+  </CountUp>
   );  
 }
 export default CounterAnimation;
