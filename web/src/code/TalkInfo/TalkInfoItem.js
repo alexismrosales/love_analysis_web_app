2bce56e2a61df1eaf0e54161988d294d773d0144
alexismrosales <aleisjorge91@gmail.com>
2024-02-07 23:56:31 -0600

    update talk info view

--- web/src/code/TalkInfo/TalkInfoItem.js
+++ web/src/code/TalkInfo/TalkInfoItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 import translate from "translate"
 
+import CounterAnimation from "../_scripts/counterAnimation";
+
 import TalkInfoData from "../../data/TalkInfoData";
 
 import Style from "./Styles.module.css";
@@ -9,15 +11,13 @@ import Style from "./Styles.module.css";
 
 function TalkInfoItem()
 {
-    
-    // Mover el hook a esta función
+    // Hook call
     const [translatedData, setTranslatedData] = React.useState(null);
 
-    // Obtener los datos de TalkInfoData
+    // Get data
     const data = TalkInfoData();
-
+  
     React.useEffect(() => {
-        // Llamar a la función asíncrona y actualizar el estado
         getTranslatedData(data).then(data => setTranslatedData(data));
     }, []);
 
@@ -30,17 +30,26 @@ function TalkInfoItem()
                     <ul>
                         <li>¿Sabías que el día en que mas hablamos es el {translatedData?.day}? 😯 </li>
                         <li>Y nuestro el mes donde más hablamos fue en {translatedData?.month} 😳</li>
-                        <li>Así como la hora donde hablamos más durante el día es a las {data.variousData[1] ? data.variousData[1].hour : []}:00 😚</li>
+                        <li>Así como la hora donde hablamos más durante el día es a las {data.variousData[0]? data.variousData[0][1].hour : ""}:00 😚</li>
                     </ul>
                 </div>
             </div>
             <div className={Style.WTMInfo}>
-                <div>text</div>
-                <div>img</div>
+            <p className="col-span-2">Y… ¿quién habla más?</p>
+            <div>
+                <p> Yo he enviado un total de  <br/><CounterAnimation limit={data.variousData[1]? data.variousData[1][1]["1"]["messages"] : ""}/> <br/> mensajes.</p>
+            </div>
+            <div> Tú has enviado un total de  <br/><CounterAnimation limit={data.variousData[1]? data.variousData[1][1]["0"]["messages"] : ""}/> <br/> mensajes. </div>
             </div>
             <div className={Style.WMInfo}>
+
+                <div>
+                    <p>
+                        En promedio enviamos {data.variousData[2]? data.variousData[2][1].byDay : ""} mensajes por día. <br/>
+                        Donde tu envías {data.variousData[2]? data.variousData[2][1].Ara : ""} mensajes y yo {data.variousData[2]? data.variousData[2][1]["~Alexis M. Rosales"] : ""} mensajes promedio por día 😎👌.
+                    </p> 
+                </div>
                 <div>img</div>
-                <div>text</div>
             </div>
         </div>
     )
@@ -51,11 +60,11 @@ async function getTranslatedData(data) {
     translate.engine = "google";
   
     const day = await translate(
-      data.variousData[0] ? data.variousData.day : "sunday",
+      data.variousData[0] ? data.variousData[0][1].day : "Sunday",
       "es"
     );
     const month = await translate(
-      data.variousData[2] ? data.variousData.month : "august",
+      data.variousData[0] ? data.variousData[0][1].month : "August",
       "es"
     );
     return { day, month };
